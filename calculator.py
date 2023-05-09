47e9215d403e24b1f80a6bb66763f4c82110ed4f
Flere21 <aaronhans2134@gmail.com>
2023-05-09 17:52:39 +0800

    pseudocode

---
+++ calculator.py
@@ -0,0 +1,10 @@
+#function for arithmetic operation
+#get user input for operation choice
+#get user input for numbers
+#if add, add numbers
+#if subtract, subtract numbers
+#if multiply, multiply numbers
+#if divide, divide numbers
+#print output
+#ask user input if want to try again
+#exception handling for invalid inputs
