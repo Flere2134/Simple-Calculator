869aab99c81eedd551843920b6e6508dfddc1662
Flere21 <aaronhans2134@gmail.com>
2023-05-09 18:23:39 +0800

    added try again function

--- calculator.py
+++ calculator.py
@@ -7,6 +7,26 @@ def multiply(num_1, num_2):
     return num_1*num_2
 def divide(num_1, num_2):
     return num_1/num_2
+#try again function
+def try_again():
+    again = None
+#asking for user input and checking it
+    while again is None:
+        answer = input('\nWould you like to try and use the calculator again? ')
+#if yes program runs again
+        if answer in ["Y", "YES", "yes", "Yes", "y"]:
+            again = True
+            continue
+#if no prints a message
+        elif answer in ["N", "NO", "no", "No", "n"]:
+            again = False
+            print('Thank you for using the calculator!')
+            break
+#if answer is invalid asks user again
+        else:
+            print('Try answering either YES or NO')
+#return value of again
+    return again
 #get user input for operation choice
 while True:
     try:
@@ -31,7 +51,8 @@ while True:
             print("Invalid input. Choose an operation by entering a number from 1 to 4.")
 #print output
         print("Result =", answer)
-#ask user input if want to try again
+#call try again function
+        again = try_again()
 #exception handling for invalid inputs
     except ZeroDivisionError:
         print("Syntax Error. Cannot be divided by zero. Please try again!")
